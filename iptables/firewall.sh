7bafd9cf3e35032b311cc973ad8e1bb7cd150821
Michael Mattie <codermattie@runbox.com>
2022-09-07 12:27:13 -0700

    update with parameter for the ssh port, ssh connection limiting, and ssh service limiting. Also delete the unload command which was garbage that had to be kept in sync with load which is impossible

--- iptables/firewall.sh
+++ iptables/firewall.sh
@@ -212,16 +212,33 @@ case $1 in
     rule -N tcp_srv_out
     rule  -A INPUT   -p tcp -j tcp_srv_in
     rule  -A OUTPUT  -p tcp -j tcp_srv_out
+
+    #
+    # tcp core connection
+    #
+
     rule  -A tcp_con_in -p tcp  -m state --state ESTABLISHED  -j ACCEPT
     rule  -A tcp_con_out -p tcp  -m state --state ESTABLISHED  -j ACCEPT
-    rule  -A tcp_con_out -p tcp  -m state --state NEW  -j ACCEPT
+
     rule  -A tcp_con_in -p tcp  -m state --state RELATED  -j ACCEPT
     rule  -A tcp_con_out -p tcp  -m state --state RELATED  -j ACCEPT
+
     rule  -A tcp_con_in -p tcp  -m conntrack --ctstatus SEEN_REPLY  --tcp-flags SYN,ACK,FIN,RST ACK,SYN  -j ACCEPT
+
     rule  -A tcp_con_in -p tcp  --tcp-flags RST RST  -j ACCEPT
     rule  -A tcp_con_out -p tcp  --tcp-flags RST RST  -j ACCEPT
     rule  -A tcp_con_in -p tcp  --tcp-flags ACK ACK  -j ACCEPT
     rule  -A tcp_con_out -p tcp  --tcp-flags ACK ACK  -j ACCEPT
+
+    if [[ "$FIREWALL_OUTBOUND_ALL" == "yes" ]]
+    then
+      rule  -A tcp_con_out -p tcp  -m state --state NEW  -j ACCEPT
+    fi
+
+    #
+    # disable tcp broadcast.
+    #
+
     rule  -A INPUT   -p tcp  -m pkttype --pkt-type broadcast  -j DROP
     rule  -A INPUT   -p tcp  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: TCP no matching rule\""
     rule  -A OUTPUT  -p tcp  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: TCP no matching rule\""
@@ -250,7 +267,12 @@ case $1 in
     rule  -A OUTPUT  -p udp -j udp_srv_out
 
     rule  -A udp_con_in -p udp  -m state --state ESTABLISHED,RELATED  -j ACCEPT
-    rule  -A udp_con_out -p udp  -m state --state ESTABLISHED,RELATED,NEW  -j ACCEPT
+    rule  -A udp_con_out -p udp  -m state --state ESTABLISHED,RELATED  -j ACCEPT
+
+    if [[ "$FIREWALL_OUTBOUND_ALL" == "yes" ]]
+    then
+      rule  -A udp_con_out -p udp -m state --state  NEW  -j ACCEPT
+    fi
 
     #
     # DROP all UDP broadcast
@@ -279,18 +301,26 @@ case $1 in
 
     if [[ "$FIREWALL_SERVER_SSH" == "yes" ]]
     then
-      rule  -A tcp_filter_in -p tcp  --dport ssh  -m connlimit --connlimit-above 20  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: SSH exceeded connection limit\""
-      rule  -A tcp_filter_in -p tcp  --dport ssh  -m state --state NEW  -m connlimit --connlimit-above 20  -j DROP
-      rule  -A tcp_srv_in -p tcp  --dport ssh  -m state --state NEW  -j ACCEPT
+      rule  -A tcp_filter_in -p tcp  --dport $FIREWALL_SSH_PORT  -m connlimit --connlimit-above $FIREWALL_SSH_CAP  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: SSH exceeded connection limit\""
+      rule  -A tcp_filter_in -p tcp  --dport $FIREWALL_SSH_PORT  -m state --state NEW  -m connlimit --connlimit-above $FIREWALL_SSH_CAP  -j DROP
+      rule  -A tcp_srv_in -p tcp  --dport $FIREWALL_SSH_PORT  -m state --state NEW -j ACCEPT
     fi
 
     if [[ "$FIREWALL_SERVER_DNS" == "yes" ]]
     then
+      rule  -A tcp_filter_in -p tcp  --dport domain  -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: SSH exceeded connection limit\""
+      rule  -A tcp_filter_in -p tcp  --dport domain -m state --state NEW  -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -j DROP
+
       rule  -A udp_srv_in -p udp  -s $EXTERNAL_NETMASK  --dport domain  -j ACCEPT
       rule  -A udp_srv_out -p udp  -d $EXTERNAL_NETMASK  --sport domain  -j ACCEPT
+    fi
+
+    if [[ "$FIREWALL_SERVER_ZONE" == "yes" ]]
+    then
+      rule  -A tcp_filter_in -p tcp  --dport domain  -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: domain exceeded connection limit\""
+      rule  -A tcp_filter_in -p tcp  --dport domain -m state --state NEW -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -j DROP
 
       rule  -A tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport domain  -m state --state NEW  -j ACCEPT
-      rule  -A tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport domain  -m state --state NEW  -j NFLOG  --nflog-group 3
     fi
 
     if [[ "$FIREWALL_SERVER_DHCP" == "yes" ]]
@@ -301,20 +331,28 @@ case $1 in
 
     if [[ "$FIREWALL_SERVER_RSYNC" == "yes" ]]
     then
+      rule  -A tcp_filter_in -p tcp  --dport rsync  -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: rsync exceeded connection limit\""
+      rule  -A tcp_filter_in -p tcp  --dport rsync -m state --state NEW -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -j DROP
+
+
       rule  -A tcp_srv_in -p tcp  -d $EXTERNAL_NETMASK  --dport rsync  -m state --state NEW  -j ACCEPT
       rule  -A tcp_srv_in -p tcp  -d $EXTERNAL_NETMASK  --dport rsync  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: RSYNC dropping unmatched\""
     fi
 
-    if [[ "$FIREWALL_SERVER_HTTP_LOW" ]]
+    if [[ "$FIREWALL_SERVER_HTTP_LOW" == "yes" ]]
     then
+      rule  -A tcp_filter_in -p tcp  --dport 80  -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: http exceeded connection limit\""
+      rule  -A tcp_filter_in -p tcp  --dport 80 -m state --state NEW -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -j DROP
+
       rule  -A tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 80  -m state --state NEW  -j ACCEPT
-      rule  -A tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 80  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: http-proxy dropping unmatched\""
     fi
 
-    if [[ "$FIREWALL_SERVER_HTTP_HIGH" ]]
+    if [[ "$FIREWALL_SERVER_HTTP_HIGH" == "yes" ]]
     then
+      rule  -A tcp_filter_in -p tcp  --dport 8080  -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: http high exceeded connection limit\""
+      rule  -A tcp_filter_in -p tcp  --dport 8080 -m state --state NEW -m connlimit --connlimit-above $FIREWALL_SERVICE_CAP  -j DROP
+
       rule  -A tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 8080  -m state --state NEW  -j ACCEPT
-      rule  -A tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 8080  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: http-proxy dropping unmatched\""
     fi
 
     #
@@ -360,6 +398,7 @@ case $1 in
     if [[ "$FIREWALL_NAT" == "yes" ]]
     then
       sysctl net.ipv4.ip_forward="1"
+
       rule  -t nat  -A POSTROUTING  -o $EXTERNAL_INTERFACE  -j MASQUERADE
 
       rule  -A FORWARD  -i $EXTERNAL_INTERFACE  -m state --state RELATED,ESTABLISHED  -j ACCEPT
@@ -385,208 +424,9 @@ case $1 in
       rule  -t nat  -A PREROUTING  -p udp  -i $EXTERNAL_INTERFACE  --dport 443  -j DNAT --to $FIREWALL_DNAT_HTTP
     fi
   ;;
-  "unload")
-    #
-    # reset kernel parameters
-    #
-
-    sysctl net.ipv4.conf.all.log_martians="0"
-    sysctl net.ipv4.ip_forward="0"
-
-    #
-    # flush the core chains
-    #
-
-    rule -P INPUT ACCEPT
-    rule -P OUTPUT ACCEPT
-    rule -P FORWARD ACCEPT
-    rule  -F INPUT
-    rule  -Z INPUT
-    rule  -F OUTPUT
-    rule  -Z OUTPUT
-    rule  -F FORWARD
-    rule  -Z FORWARD
-    rule  -t nat  -F POSTROUTING
-    rule  -t nat  -Z POSTROUTING
-    rule  -t nat  -F PREROUTING
-    rule  -t nat  -Z PREROUTING
-
-    #
-    # flush loopback
-    #
-
-    rule  -D INPUT -i lo -j ACCEPT
-    rule  -D OUTPUT -o lo -j ACCEPT
-
-    #
-    # institute block loopback spoofing
-    #
-
-    rule  -D INPUT  -i $EXTERNAL_INTERFACE  -s $LOOPBACK_NETMASK  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: spoof attempt\""
-    rule  -D INPUT  -i $EXTERNAL_INTERFACE  -s $LOOPBACK_NETMASK  -j DROP
-    rule  -D INPUT  -i $EXTERNAL_INTERFACE  -s $EXTERNAL_NETMASK  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: spoof attempt\""
-    rule  -D INPUT  -i $EXTERNAL_INTERFACE  -s $EXTERNAL_NETMASK  -j DROP
-
-    rule  -D INPUT  -i $EXTERNAL_INTERFACE  -s $EXTERNAL_NETMASK  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: spoof attempt\""
-    rule  -D INPUT  -i $EXTERNAL_INTERFACE  -s $EXTERNAL_NETMASK -j DROP
-
-
-    # delete ICMP
-
-    rule  -F icmp_filter_in
-    rule  -Z icmp_filter_in
-    rule  -F icmp_filter_out
-    rule  -Z icmp_filter_out
-    rule  -F icmp_traffic_in
-    rule  -Z icmp_traffic_in
-    rule  -F icmp_traffic_out
-    rule  -Z icmp_traffic_out
-
-    rule  -D INPUT  -p icmp -j icmp_filter_in
-    rule  -D OUTPUT -p icmp -j icmp_filter_out
-    rule  -D INPUT  -p icmp -j icmp_traffic_in
-    rule  -D OUTPUT -p icmp -j icmp_traffic_out
-
-    # delete ICMP filtering
-
-    rule  -D icmp_filter_in  -p icmp  -m pkttype --pkt-type broadcast  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: ICMP broadcast!\""
-    rule  -D icmp_filter_in  -p icmp  -m pkttype --pkt-type broadcast  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: ICMP broadcast!\""
-    rule  -D icmp_filter_in  -p icmp  -m pkttype --pkt-type broadcast  -j DROP
-    rule  -D icmp_filter_in  -p icmp  -m pkttype --pkt-type broadcast  -j DROP
-    rule  -D icmp_filter_in  -p icmp  --icmp-type timestamp-request  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix="\"firewall: ICMP strange\""
-    rule  -D icmp_filter_in  -p icmp  --icmp-type timestamp-reply  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix="\"firewall: ICMP strange\""
-    rule  -D icmp_filter_in  -p icmp  --icmp-type address-mask-request  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix="\"firewall: ICMP strange\""
-    rule  -D icmp_filter_out -p icmp  --icmp-type address-mask-reply  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix="firewall: ICMP strange "
-    rule  -D icmp_filter_in  -p icmp  --icmp-type timestamp-request  -j DROP
-    rule  -D icmp_filter_in  -p icmp  --icmp-type timestamp-reply  -j DROP
-    rule  -D icmp_filter_in  -p icmp  --icmp-type address-mask-request  -j DROP
-    rule  -D icmp_filter_out -p icmp  --icmp-type address-mask-reply  -j DROP
-    rule  -D icmp_traffic_in  -p icmp  --icmp-type echo-request  -m limit --limit 8\/second --limit-burst 24  -j ACCEPT
-    rule  -D icmp_traffic_in  -p icmp  --icmp-type echo-reply  -m limit --limit 8\/second --limit-burst 24  -j ACCEPT
-    rule  -D icmp_traffic_out -p icmp  --icmp-type echo-request  -j ACCEPT
-    rule  -D icmp_traffic_out -p icmp  --icmp-type echo-reply  -j ACCEPT
-    rule  -D icmp_traffic_in  -p icmp  --icmp-type echo-reply  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: ICMP high rate\""
-    rule  -D icmp_traffic_in  -p icmp  --icmp-type echo-request  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "firewall: ICMP high rate "
-    rule  -D icmp_traffic_in  -p icmp  --icmp-type echo-reply  -j DROP
-    rule  -D icmp_traffic_in  -p icmp  --icmp-type echo-request  -j DROP
-    rule  -D icmp_traffic_in  -p icmp  -m state --state RELATED  -j ACCEPT
-    rule  -D icmp_traffic_out -p icmp  -m state --state RELATED  -j ACCEPT
-    rule  -D icmp_traffic_out -p icmp  -m state --state NEW  -j ACCEPT
-    rule  -D INPUT  -p icmp  -j NFLOG --nflog-group 2  --nflog-prefix="\"firewall: ICMP no rule for packet\""
-    rule  -D OUTPUT -p icmp  -j NFLOG --nflog-group 2  --nflog-prefix="\"firewall: ICMP no rule for packet\""
-    rule  -D INPUT  -p icmp  -j DROP
-
-    #
-    # delete core TCP chains
-    #
-
-    rule  -F tcp_filter_in
-    rule  -Z tcp_filter_in
-    rule  -F tcp_filter_out
-    rule  -Z tcp_filter_out
-    rule  -D INPUT   -p tcp -j tcp_filter_in
-    rule  -D OUTPUT  -p tcp -j tcp_filter_out
-    rule  -F tcp_con_in
-    rule  -Z tcp_con_in
-    rule  -F tcp_con_out
-    rule  -Z tcp_con_out
-    rule  -D INPUT   -p tcp -j tcp_con_in
-    rule  -D OUTPUT  -p tcp -j tcp_con_out
-    rule  -F tcp_srv_in
-    rule  -Z tcp_srv_in
-    rule  -F tcp_srv_out
-    rule  -Z tcp_srv_out
-    rule  -D INPUT   -p tcp -j tcp_srv_in
-    rule  -D OUTPUT  -p tcp -j tcp_srv_out
-    rule  -D tcp_con_in -p tcp  -m state --state ESTABLISHED  -j ACCEPT
-    rule  -D tcp_con_out -p tcp  -m state --state ESTABLISHED  -j ACCEPT
-    rule  -D tcp_con_out -p tcp  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_con_in -p tcp  -m state --state RELATED  -j ACCEPT
-    rule  -D tcp_con_out -p tcp  -m state --state RELATED  -j ACCEPT
-    rule  -D tcp_con_in -p tcp  -m conntrack --ctstatus SEEN_REPLY  --tcp-flags SYN,ACK,FIN,RST ACK,SYN  -j ACCEPT
-    rule  -D tcp_con_in -p tcp  --tcp-flags RST RST  -j ACCEPT
-    rule  -D tcp_con_out -p tcp  --tcp-flags RST RST  -j ACCEPT
-    rule  -D tcp_con_in -p tcp  --tcp-flags ACK ACK  -j ACCEPT
-    rule  -D tcp_con_out -p tcp  --tcp-flags ACK ACK  -j ACCEPT
-    rule  -D INPUT   -p tcp  -m pkttype --pkt-type broadcast  -j DROP
-    rule  -D INPUT   -p tcp  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: TCP no matching rule\""
-    rule  -D OUTPUT  -p tcp  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: TCP no matching rule\""
-    rule  -D INPUT   -p tcp  -j DROP
-    rule  -F udp_filter_in
-    rule  -Z udp_filter_in
-    rule  -F udp_filter_out
-    rule  -Z udp_filter_out
-    rule  -D INPUT   -p udp -j udp_filter_in
-    rule  -D OUTPUT  -p udp -j udp_filter_out
-    rule  -F udp_con_in
-    rule  -Z udp_con_in
-    rule  -F udp_con_out
-    rule  -Z udp_con_out
-    rule  -D INPUT   -p udp -j udp_con_in
-    rule  -D OUTPUT  -p udp -j udp_con_out
-    rule  -F udp_srv_in
-    rule  -Z udp_srv_in
-    rule  -F udp_srv_out
-    rule  -Z udp_srv_out
-    rule  -D INPUT   -p udp -j udp_srv_in
-    rule  -D OUTPUT  -p udp -j udp_srv_out
-    rule  -D udp_con_in -p udp  -m state --state ESTABLISHED,RELATED  -j ACCEPT
-    rule  -D udp_con_out -p udp  -m state --state ESTABLISHED,RELATED,NEW  -j ACCEPT
-    rule  -D udp_con_in -p udp  --sport domain  -j ACCEPT
-    rule  -D INPUT   -p udp  -m pkttype --pkt-type broadcast  -j DROP
-    rule  -D INPUT   -p udp  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: UDP no rule for packet\""
-    rule  -D OUTPUT  -p udp  -m limit --limit 24\/minute  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: UDP no rule for packet\""
-    rule  -D INPUT   -p udp  -j DROP
-    rule  -D udp_filter_in -p udp  -i $EXTERNAL_INTERFACE  -s 0.0.0.0  --sport 67  -m pkttype --pkt-type broadcast  -j DROP
-    rule  -D udp_con_out -p udp  -o $EXTERNAL_INTERFACE  --dport 67  -j ACCEPT
-    rule  -D udp_con_in -p udp  -i $EXTERNAL_INTERFACE  ! -s 0.0.0.0  --sport 67  -m pkttype --pkt-type broadcast  -j ACCEPT
-    rule  -D tcp_filter_in -p tcp  --dport ssh  -m connlimit --connlimit-above 20  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: SSH exceeded connection limit\""
-    rule  -D tcp_filter_in -p tcp  --dport ssh  -m state --state NEW  -m connlimit --connlimit-above 20  -j DROP
-    rule  -D tcp_srv_in -p tcp  --dport ssh  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  --dport ssh  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: SSH dropping unmatched\""
-    rule  -D udp_srv_in -p udp  -s $EXTERNAL_NETMASK  --dport domain  -j ACCEPT
-    rule  -D udp_srv_out -p udp  -d $EXTERNAL_NETMASK  --sport domain  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport domain  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport domain  -m state --state NEW  -j NFLOG  --nflog-group 3
-    rule  -D udp_srv_out -p udp  -d $EXTERNAL_NETMASK  --sport 67  -j ACCEPT
-    rule  -D udp_srv_in -p udp  -s $EXTERNAL_NETMASK  --sport 68  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport rsync  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport rsync  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: RSYNC dropping unmatched\""
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 8080  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 8080  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: http-proxy dropping unmatched\""
-    rule  -D udp_srv_in -p udp  -s $EXTERNAL_NETMASK  --dport domain  -j ACCEPT
-    rule  -D udp_srv_out -p udp  -d $EXTERNAL_NETMASK  --sport domain  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport domain  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport domain  -m state --state NEW  -j NFLOG  --nflog-group 3
-    rule  -D udp_srv_out -p udp  -d $EXTERNAL_NETMASK  --sport 67  -j ACCEPT
-    rule  -D udp_srv_in -p udp  -s $EXTERNAL_NETMASK  --sport 68  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 8080  -m state --state NEW  -j ACCEPT
-    rule  -D tcp_srv_in -p tcp  -s $EXTERNAL_NETMASK  --dport 8080  -m state --state NEW  -j NFLOG  --nflog-group 3  --nflog-prefix "\"firewall: http-proxy dropping unmatched\""
-    rule  -D udp_filter_in -p udp  --dport 137:139  -j DROP
-    rule  -D udp_filter_in -p udp  --dport 137:139  -j REJECT
-    rule  -D udp_filter_in -p udp  --dport 137:139  -j REJECT  --reject-with icmp-host-unreachable
-    rule  -D tcp_filter_in -p tcp  --dport 1433  -j DROP
-    rule  -D udp_filter_in -p udp  -i $EXTERNAL_INTERFACE  --dport domain  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport domain  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport http  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport https  -j DROP
-    rule  -D udp_filter_in -p udp  -i $EXTERNAL_INTERFACE  --dport sunrpc  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport sunrpc  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport telnet  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport ftp  -j DROP
-    rule  -D tcp_filter_in -p tcp  -i $EXTERNAL_INTERFACE  --dport ftp-data  -j DROP
-    rule  -t nat  -D POSTROUTING  -o $EXTERNAL_INTERFACE  -j MASQUERADE
-    rule  -D FORWARD  -i $EXTERNAL_INTERFACE  -m state --state RELATED,ESTABLISHED  -j ACCEPT
-    rule  -D FORWARD  -p udp  -j ACCEPT
-    rule  -D FORWARD  -p icmp  -j ACCEPT
-    rule  -D FORWARD  -j NFLOG --nflog-group 2  --nflog-prefix "\"firewall: unknown FORWARD traffic will be dropped\""
-
-    rule  -D FORWARD  -s $EXTERNAL_NETMASK  -j ACCEPT
-    rule  -D FORWARD  -s $EXTERNAL_NETMASK  -j ACCEPT
-  ;;
   "reload")
-    $0 disable
     $0 flush
+    $0 delete
     $0 load
   ;;
   "help"|*)
@@ -597,7 +437,6 @@ delete   =  delete chains
 flush    = flush chains
 open     = open firewall completely, default accept policy
 load     = load the firewall rules
-unload   = remove firewall rules
 reload   = re-initialize the firewall
 HELP
   ;;
