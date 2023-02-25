528dcb0917800347067e9b6d14f1aa8b8a707df5
Michael Mattie <codermattie@runbox.com>
2023-02-24 16:47:42 -0800

    (feat) first draft of gatekeeper firewall

---
+++ pf/gatekeeper.sh
@@ -0,0 +1,41 @@
+#! /usr/bin/env bash
+
+pf_firewall=$HOME/code/firewall/pf/openbsd.sh
+
+wifi="iwx0"
+
+NTP=123
+IRCD=6667
+
+source $pf_firewall
+
+#
+# basics policy
+#
+
+default_policy
+
+#
+# WiFi (local)
+#
+
+# low level
+
+open_icmp $wifi
+open_dhcp $wifi
+
+# basic services
+
+open_out $wifi udp domain
+open_out $wifi udp $NTP
+
+# security
+
+open_out $wifi tcp ssh
+open_out $wifi tcp 6666
+
+# ssh server
+
+open_server_throttle $wifi tcp 6666 10 "5/10"
+open_server_throttle $wifi tcp $IRCD 10 "5/10"
+
