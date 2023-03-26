211b2012b640f54839e31bd3b4f9f4b4fc31050d
Michael Mattie <codermattie@runbox.com>
2023-03-26 13:22:12 -0700

    (feat) update hades with latest iptables changes

--- iptables/hades.sh
+++ iptables/hades.sh
@@ -3,80 +3,52 @@
 # Generic Firewall config for simple client host
 #
 
-WIFI=wlan0
-ETHERNET=eth2
+wifi=wlan0
+ethernet=eth2
 VPN=tun0
 
-WIRELESS="192.168.10.0/24"
-WIRED="192.168.24.0/24"
+wirelessNet="192.168.10.0/24"
+wiredNet="192.168.24.0/24"
+
+wirelessIP="1921.68.10.169"
+wiredIP="192.168.24.7"
 
 SSH="ssh,6666"
 RDP="3389"
 
+# universal
+
+icmp_core
+icmp_block_strange
+icmp_block_broadcast
+
+tcp_core
+udp_core
+
 # Wi-Fi
 
-icmp_core $WIFI
+icmp_ping_throttle $wirelessIP
+icmp_trusted $wirelessIP
 
-icmp_block_broadcast $WIFI
-icmp_block_strange $WIFI
-icmp_ping_throttle $WIFI
+tcp_drop_broadcast $wifi
+tcp_any_out $wirelessIP
 
-tcp_core $WIFI
-tcp_drop_broadcast $WIFI
+udp_any_out $wirelessIP
 
-tcp_any_out $WIFI
+open_dhcp $wifi
 
-udp_core $WIFI
-udp_drop_broadcast $WIFI
-
-udp_any_out $WIFI
-
-open_dhcp $WIFI
-
-# public servers
-
-open_tcp_server $WIRELESS $SSH 10
-open_tcp_server $WIRELESS 80 10
+open_tcp_server $wirelessIP $wirelessNet $SSH 10
 
 # Ethernet
 
-icmp_core $ETHERNET
+icmp_ping_throttle $wiredIP
+icmp_trusted $wiredIP
 
-icmp_block_broadcast $ETHERNET
-icmp_block_strange $ETHERNET
-icmp_ping_throttle $ETHERNET
+tcp_drop_broadcast $ethernet
+tcp_any_out $wiredIP
 
-tcp_core $ETHERNET
-tcp_drop_broadcast $ETHERNET
-tcp_any_out $ETHERNET
+udp_drop_broadcast $ethernet
+udp_any_out $wiredIP
 
-udp_core $ETHERNET
-udp_drop_broadcast $ETHERNET
-
-udp_any_out $ETHERNET
-
-#
-# VPN
-#
-
-icmp_core $VPN
-
-icmp_block_broadcast $VPN
-icmp_block_strange $VPN
-icmp_ping_throttle $VPN
-
-tcp_core $VPN
-tcp_drop_broadcast $VPN
-tcp_any_out $VPN
-
-udp_core $VPN
-udp_drop_broadcast $VPN
-udp_any_out $VPN
-
-open_dhcp $VPN
-
-# local servers
-
-open_tcp_server $WIRED $SSH 10
-open_tcp_server $WIRED 80 10
-open_tcp_server $WIRED $RDP 10
+open_tcp_server $wiredIP $wiredNet $SSH 10
+open_tcp_server $wiredIP $wiredNet $RDP 10
