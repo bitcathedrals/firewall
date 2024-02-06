ab74c46ab07e948c890befe4a5c69eeaf5b8386b
Michael Mattie <codermattie@runbox.com>
2024-02-06 11:25:31 -0800

    (feat) open a port for the python package repository

--- pf/gatekeeper.sh
+++ pf/gatekeeper.sh
@@ -11,6 +11,8 @@ macro_host hadesWired "hades.wired"
 macro_host redboxWired "redbox.wired"
 macro_host spartanWired "spartan.wired"
 
+macro python 8080
+
 rpc_map localhost
 
 #
@@ -35,6 +37,7 @@ outbound \$wirelessIP tcp "$WEB"
 
 in_from \$wirelessIP tcp "$SSH" \$wirelessNet "5/1" 10
 in_from \$wirelessIP "{ udp , tcp }" $DOMAIN \$wirelessNet "50/1" 150
+in_from \$wirelessIP "tcp" \$python \$wirelessNet "50/1" 10
 
 # Wired
 
@@ -44,9 +47,12 @@ open_trusted \$wiredIP
 
 in_from \$wiredIP tcp "$SSH" \$wiredNet "5/1" 10
 in_from \$wiredIP "{ udp , tcp }" $DOMAIN \$wiredNet "50/1" 150
+
 in_from \$wiredIP tcp $RSYNC "{ \$crackerWired , \$hadesWired , \$redboxWired , \$spartanWired }" "5/1" 10
 
 in_from \$wiredIP udp "{ $PORTMAP_UDP , $STATUS_UDP , $LOCK_UDP , $MOUNT_UDP , $NFS_UDP }" \$wiredNet "30/10" "25"
 in_from \$wiredIP tcp "{ $PORTMAP_TCP , $STATUS_TCP , $LOCK_TCP , $MOUNT_TCP , $NFS_TCP }" \$wiredNet "30/10" "25"
+
+in_from \$wiredIP "tcp" \$python \$wirelessNet "50/1" 10
 
 
